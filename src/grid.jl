7ea40edbad4978ea137f592c6e49984db8bd4cb4
georgeglidden <gliddenegeorge@gmail.com>
2021-11-28 20:48:05 -0700

    fixed grid refinement, recursive query logic

--- src/grid.jl
+++ src/grid.jl
@@ -8,73 +8,40 @@ struct GridStructure
     shape::Tuple{Float64,Float64}
 end
 
-mutable struct Substructure
-    cardinality::Int64
-    center::Tuple{Float64,Float64}
-    shape::Tuple{Float64,Float64}
-end
-
 struct Grid
     rootdir::String
     tiles::Dict{Tuple{Int64,Int64}, IOStream}
     structure::GridStructure
-    substructure::Dict{Tuple{Int64,Int64}, Substructure}
+    tilecardinality::Matrix{Int64}
     function Grid(rootdir, resolution, cardinality, spatialindex, center, shape)
         structure = GridStructure(resolution, cardinality, spatialindex,
             center, shape)
         tiles = Dict{Tuple{Int64,Int64}, IOStream}()
-        substructure = Dict{Tuple{Int64,Int64}, Substructure}()
-        return new(rootdir, tiles, structure, substructure)
+        tilecardinality = zeros(Int64, (resolution, resolution))
+        return new(rootdir, tiles, structure, tilecardinality)
     end
 end
 
-"Associate a position tuple to its tile index."
-function index(position::Tuple{Float64,Float64}, structure::GridStructure,
-        epsilon::Float64 = 1e-10)
-    relpos = ((position .- structure.center) ./ ((1 + epsilon) .* structure.shape)) .+ 0.5
-    fzyidx = relpos .* structure.resolution
-    return (floor(Int64, fzyidx[1]), floor(Int64, fzyidx[2]))
+function corner(structure::GridStructure)
+    center = structure.center
+    shape = structure.shape
+    return center .- (shape ./ 2)
 end
 
-"Wraps index(position::Tuple{Float64,Float64}, structure::GridStructure, ..."
-function index(position::Tuple{Float64,Float64}, grid::Grid,
-        epsilon::Float64 = 1e-10)
-    return index(position, grid.structure, epsilon)
+function corner(grid::Grid)
+    return corner(grid.structure)
 end
 
-# https://stackoverflow.com/questions/64957524/how-can-i-obtain-the-complement-of-list-of-indexes-in-julia
-function mysetdiff(y, x)
-    res = Vector{eltype(y)}(undef, length(y) - length(x))
-    i = 1
-    @inbounds for el in y
-        el ∈ x && continue
-        res[i] = el
-        i += 1
-    end
-
-    res
+function tilebounds(structure::GridStructure, idx::Tuple{Int64, Int64})
+    gridcorner = corner(structure)
+    shape = grid.structure.shape ./ grid.structure.resolution
+    corner = gridcorner .+ (idx .* tileshape)
+    center = tilecorner .+ (tileshape ./ 2)
+    return shape, center, corner
 end
 
-""
-function formatrow(x, leadingindex::Vector{Int64})
-    return join(x, ',')*'\n'
-end
-
-function tilelabel(rootdir::String, idx::Tuple{Int64, Int64})
-    return join([rootdir, string("r", idx[1], "c", idx[2])], '/')
-end
-
-function write_and_open!(grid::Grid, idx::Tuple{Int64, Int64}, row)
-    if !haskey(grid.tiles, idx)
-        path = tilelabel(grid.rootdir, idx)*".csv"
-        grid.tiles[idx] = open(path, "w")
-        n = grid.structure.resolution
-        shape = grid.structure.shape ./ n
-        center = grid.structure.shape .* (idx ./ n) .+ (shape ./ 2)
-        grid.substructure[idx] = Substructure(0, shape, center)
-    end
-    write(grid.tiles[idx], row)
-    grid.substructure[idx].cardinality += 1
+function cardinality(grid::Grid, idx::Tuple{Int64, Int64})
+    return grid.tilecardinality[1+idx[1],1+idx[2]]
 end
 
 function format_metadata(structure::GridStructure)
@@ -99,15 +66,62 @@ function read_metadata(rootdir::String)
         return DelimitedFiles.readdlm(f)
     end
 end
+function read_structure(rootdir::String)
+    mat = read_metadata(rootdir)
+    return GridStructure(mat[1,1],mat[1,2],mat[2,:],mat[3,:],mat[4,:])
+end
+
+"Associate a position tuple to its tile index."
+function index(position::Tuple{Float64,Float64}, structure::GridStructure, epsilon::Float64 = 1e-10)
+    relpos = ((position .- structure.center) ./ ((1 + epsilon) .* structure.shape)) .+ 0.5
+    fzyidx = relpos .* structure.resolution
+    return (floor(Int64, fzyidx[1]), floor(Int64, fzyidx[2]))
+end
+
+"Wraps index"
+function index(position::Tuple{Float64,Float64}, grid::Grid,
+        epsilon::Float64 = 1e-10)
+    return index(position, grid.structure, epsilon)
+end
+
+# https://stackoverflow.com/questions/64957524/how-can-i-obtain-the-complement-of-list-of-indexes-in-julia
+function mysetdiff(y, x)
+    res = Vector{eltype(y)}(undef, length(y) - length(x))
+    i = 1
+    @inbounds for el in y
+        el ∈ x && continue
+        res[i] = el
+        i += 1
+    end
+
+    res
+end
+
+function format_row(x, leadingindex::Vector{Int64})
+    return join(x, ',')*'\n'
+end
+
+function tilelabel(rootdir::String, idx::Tuple{Int64, Int64})
+    return join([rootdir, string("r", idx[1], "c", idx[2])], '/')
+end
+
+function write_and_open!(grid::Grid, idx::Tuple{Int64, Int64}, row)
+    if !haskey(grid.tiles, idx)
+        path = tilelabel(grid.rootdir, idx)*".csv"
+        grid.tiles[idx] = open(path, "w")
+    end
+    write(grid.tiles[idx], row)
+    grid.tilecardinality[1+idx[1],1+idx[2]] += 1
+end
 
 function write!(grid::Grid, row)
     pos = Tuple{Float64, Float64}(row[grid.structure.spatialindex])
     idx = index(pos, grid)
-    frow = formatrow(row, grid.structure.spatialindex)
+    frow = format_row(row, grid.structure.spatialindex)
     write_and_open!(grid, idx, frow)
 end
 
-function writegrid!(grid::Grid, rows)
+function write_grid!(grid::Grid, rows)
     #map(x -> write_to_tile!(grid, x), rows)
     for x in rows
         write!(grid, x)
@@ -130,7 +144,7 @@ end
 
 "Accumulate chars until `ldf.delim` is encountered, then split by `ldf.sep`."
 function Base.iterate(ldf::LazyDelimitedFile, state=missing)
-    item = ""::String
+    item = ""
     while !eof(ldf.file)
         chr = read(ldf.file, Char)
         if chr==ldf.delim
@@ -141,56 +155,60 @@ function Base.iterate(ldf::LazyDelimitedFile, state=missing)
     end
 end
 
-"."
-function parserow!(row, index::Vector{Int64})
-    for i in index
-        row[i] = parse(Float64, row[i])
+function parse_row!(row, index::Vector{Int64})
+    parsed_row = Vector{Any}()
+    for i in 1:length(row)
+        if i in index
+            val = parse(Float64, row[i])
+        else
+            val = row[i]
+        end
+        push!(parsed_row, val)
     end
-    return row
+    return parsed_row
 end
 
 function tiledata(f::IOStream, spatialindex::Vector{Int64})
-    return Iterators.map(x -> parserow!(x, spatialindex),
-        LazyDelimitedFile(f, '\n', ','))
+    return Iterators.map(x -> parse_row!(x, spatialindex), LazyDelimitedFile(f, '\n', ','))
 end
 
-function refinetile(grid::Grid, idx::Tuple{Int64, Int64}, resolution::Int64,
-        spatialindex::Vector{Int64})
-    substructure = grid.substructure[idx]
-    n = substructure.cardinality
-    subcenter = substructure.center
-    subshape = substructure.shape
+function decompose_tile(grid::Grid, idx::Tuple{Int64, Int64}, resolution::Int64, spatialindex::Vector{Int64})
+    tileshape, tilecenter = tilebounds(grid.structure, idx)[1:2]
     label = tilelabel(grid.rootdir, idx)
     subdir = mkdir(label)
-    subgrid = Grid(subdir, resolution, n, spatialindex, subcenter, subshape)
+    subgrid = Grid(subdir, resolution, cardinality(grid, idx), spatialindex, tilecenter, tileshape)
     path = label*".csv"
     open(path, "r") do tilefile
         data = tiledata(tilefile, spatialindex)
-        writegrid!(subgrid, data)
+        write_grid!(subgrid, data)
     end
     rm(path)
     return subgrid
 end
 
-function refinegrid(grid::Grid, threshold::Int64, resolution::Int64,
-        spatialindex::Vector{Int64})
+function refine_grid(grid::Grid, threshold::Int64, resolution::Int64, spatialindex::Vector{Int64})
     subgrids = Grid[]
     for idx in keys(grid.tiles)
-        n = grid.substructure[idx].cardinality
+        n = cardinality(grid, idx)
         if n > threshold
-            refinedgrid = refinetile(grid, idx, resolution, spatialindex)
+            refinedgrid = decompose_tile(grid, idx, resolution, spatialindex)
             push!(subgrids, refinedgrid)
         end
     end
     return subgrids
 end
 
-function dotest()
-    n = 5000
+function testgrid()
+    n = 3500
     r = 9
     points = ((x, y, "meta", 'd', 4, 't', 4) for x in 0:n for y in 0:n)
+    if isdir("db")
+        rm("db", recursive=true)
+    end
+    mkdir("db")
     grid = Grid("db", r, (n+1)^2, [1, 2], (n/2, n/2), (n,n))
-    print("writing ", (n+1)^2, " rows...")
-    @time writegrid!(grid, points)
-    @time refinegrid(grid, parse(Int64, grid.structure.cardinality / (r^2)), r)
+    println("writing ", (n+1)^2, " rows...")
+    write_grid!(grid, points)
+    println("refining grid")
+    refine_grid(grid, round(Int64, grid.structure.cardinality / (2*r^2)), r, [1, 2])
 end
--- src/query.jl
+++ src/query.jl
@@ -8,12 +8,16 @@ end
 function querytiles(query, metadata)
     resolution = metadata[1]
     width, height = metadata[4,:]
+    left, top = metadata[3,:] .- (metadata[4,:] ./ 2)
+    println("\tresolution ", (width, height))
     queryleft, queryright = query.interval[1,:]
-    leftidx = max(round(Int64, queryleft*resolution/width), 0)
-    rightidx = round(Int64, queryright*resolution/width)
+    leftidx = max(round(Int64, resolution*(queryleft-left)/width), 0)
+    rightidx = round(Int64, resolution*(queryright-left)/width)
     querytop, querybottom = query.interval[2,:]
-    topidx = max(round(Int64, querytop*resolution/height), 0)
-    bottomidx = round(Int64, querybottom*resolution/height)
+    topidx = max(round(Int64, resolution*(querytop-top)/height), 0)
+    bottomidx = round(Int64, resolution*(querybottom-top)/height)
+    println("\tquery interval", (queryleft, queryright, querytop, querybottom))
+    println("\tquery indices", (leftidx, rightidx, topidx, bottomidx))
     return ((r,c) for r in leftidx:rightidx for c in topidx:bottomidx)
 end
 
@@ -35,11 +39,12 @@ end
 "Recursively solve for the comprising tiles of a query, by BFS traversal of grid directories"
 function resolve_tiles(query::RectangularQuery)
     metadata = read_metadata(query.rootdir)
+    println("dir ", query.rootdir, " meta ", metadata)
     tilepaths = String[]
     for idx in querytiles(query, metadata)
         path = tilelabel(query.rootdir, idx)
         if isdir(path)
-            #println("idx ", path, " is a subgrid")
+            println("idx ", path, " is a subgrid")
             # restrict query to subgrid
             subquery = restrict_query(query, path)
             # recurse
@@ -49,10 +54,10 @@ function resolve_tiles(query::RectangularQuery)
             # terminate recursion
             path *= ".csv"
             if isfile(path)
-                #println("idx ", path, " is a file")
+                println("idx ", path, " is a file")
                 push!(tilepaths, path)
-            #else
-            #    println("idx ", path, " does not exist")
+            else
+                println("idx ", path, " does not exist")
             end
         end
     end
@@ -63,7 +68,7 @@ function serve(query::RectangularQuery)
     paths = resolve_tiles(query)
 end
 
-function dotest(xb, yb)
+function testquery(xb, yb)
     interval = Matrix{Float64}(undef, (2,2))
     interval[1,:] = xb
     interval[2,:] = yb
